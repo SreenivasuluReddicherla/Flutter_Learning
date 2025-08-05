4c9f5be377e6bab1b61a08f88ea0c37bac749567
SreenivasuluReddicherla <sreenivasreddicherla@gmail.com>
2025-08-05 17:34:30 +0530

    Form Fields

--- lib/views/screens/authentication_screens/login_screen.dart
+++ lib/views/screens/authentication_screens/login_screen.dart
@@ -7,35 +7,113 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Login Your account",
-              style: GoogleFonts.getFont(
-                'Lato',
-                color: Color(0xFF0d120E),
-                fontWeight: FontWeight.bold,
-                letterSpacing: 0.2,
-                fontSize: 23,
+      backgroundColor: Colors.white.withAlpha(222),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Login Your account",
+                style: GoogleFonts.getFont(
+                  'Lato',
+                  color: Color(0xFF0d120E),
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.2,
+                  fontSize: 23,
+                ),
               ),
-            ),
-            Text(
-              "To explore the world exclusively",
-              style: GoogleFonts.getFont(
-                'Lato',
-                color: Color.fromARGB(255, 78, 0, 0),
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+              Text(
+                "To explore the world exclusively",
+                style: GoogleFonts.getFont(
+                  'Lato',
+                  color: Color.fromARGB(255, 78, 0, 0),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Image.asset(
-              'assets/images/Illustration.png',
-              width: 200,
-              height: 200,
-            ),
-          ],
+              Image.asset(
+                'assets/images/Illustration.png',
+                width: 200,
+                height: 200,
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Email',
+                  style: GoogleFonts.getFont(
+                    'Nunito Sans',
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 0.2,
+                  ),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                  ),
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  labelText: "enter your email",
+                  labelStyle: GoogleFonts.getFont(
+                    "Nunito Sans",
+                    fontSize: 14,
+                    letterSpacing: 0.1,
+                  ),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset(
+                      'assets/icons/email.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Password',
+                  style: GoogleFonts.getFont(
+                    'Nunito Sans',
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 0.2,
+                  ),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                  ),
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  labelText: "enter your password",
+                  labelStyle: GoogleFonts.getFont(
+                    "Nunito Sans",
+                    fontSize: 14,
+                    letterSpacing: 0.1,
+                  ),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset(
+                      'assets/icons/password.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                  suffixIcon: Icon(Icons.visibility),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
